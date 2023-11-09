ff10c852a7ffb22040dce4a5144e3ceec2699bc2
Marc <marcbaque1311@gmail.com>
2023-11-09 21:29:51 +0100

    [ ADD ] get user data from cloud fn

--- supabase/functions/user-data/index.ts
+++ supabase/functions/user-data/index.ts
@@ -3,29 +3,40 @@ import type { Database } from '../../../src/supabase/types/database.ts';
 import type { DbResult } from '../../../src/supabase/types/helpers.ts';
 
 Deno.serve(async (req) => {
-  const { phone } = await req.json();
-  const data = {
-    message: `Hello ${phone}!`,
-  };
+  const { phone, pin } = await req.json();
 
   const supabaseClient = createClient<Database>(
-    // Supabase API URL - env var exported by default.
     Deno.env.get('SUPABASE_URL') ?? '',
-    // Supabase API ANON KEY - env var exported by default.
     Deno.env.get('SUPABASE_ANON_KEY') ?? '',
   );
 
   const q = supabaseClient
     .from('users')
-    .select('pin')
+    .select(`
+      phone,
+      pin,
+      portfolios (
+        name,
+        isins,
+        orders (
+          isin,
+          amount,
+          year,
+          month,
+          day,
+          executed_at
+        )
+      )
+    `)
     .eq('phone', phone)
+    .eq('pin', pin)
     .single();
 
   const user: DbResult<typeof q> = await q;
 
   if (user.error) throw user.error;
 
-  return new Response(JSON.stringify({ pin: user.data.pin, data }), {
+  return new Response(JSON.stringify({ user: user.data }), {
     headers: { 'Content-Type': 'application/json' },
     status: 200,
   });
