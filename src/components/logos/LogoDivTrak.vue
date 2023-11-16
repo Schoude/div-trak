92ca24a6c28c5f65efe422f5cef6a60c52eb493a
Marc <marcbaque1311@gmail.com>
2023-11-17 00:39:30 +0100

    [ FIX ] public import path

--- src/components/logos/LogoDivTrak.vue
+++ src/components/logos/LogoDivTrak.vue
@@ -3,5 +3,5 @@ withDefaults(defineProps<{width: number}>(), { width: 50 });
 </script>
 
 <template>
-  <img :width="width" src="logo-div-trak.png" alt="Div Trak Logo">
+  <img :width="width" src="/logo-div-trak.png" alt="Div Trak Logo">
 </template>
