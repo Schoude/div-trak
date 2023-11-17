1c05237e5984234cbebfce31218534a30718eedf
Marc <marcbaque1311@gmail.com>
2023-11-17 22:24:57 +0100

    [ ADD ] FormLogin comp

---
+++ src/components/forms/FormLogin.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+import LogoDivTrak from '@/components/logos/LogoDivTrak.vue';
+import { useAuthStore } from '@/stores/auth';
+import { reactive } from 'vue';
+
+const authStore = useAuthStore();
+
+const loginData = reactive({ phone: '', pin: '' });
+
+function onSubmit () {
+  authStore.login(loginData);
+}
+</script>
+
+<template>
+<div class="form-login">
+  <LogoDivTrak :width="250" />
+  <p class="cta text-l">Enter your credentials</p>
+  <form @submit.prevent="onSubmit">
+    <label for="phone">
+      <div class="label">Phone</div>
+      <input v-model="loginData.phone" id="phone" type="text" placeholder="+49123456789">
+    </label>
+    <label for="pin">
+      <div class="label">PIN</div>
+      <input v-model="loginData.pin" id="pin" type="text" placeholder="8888">
+    </label>
+    <button>Einloggen</button>
+  </form>
+</div>
+</template>
+
+<style lang='scss' scoped>
+.cta {
+  margin-block-end: .5rem;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,50 +1,12 @@
 <script setup lang='ts'>
-import { useAuthStore } from '@/stores/auth';
-import { reactive } from 'vue';
-import LogoDivTrak from '@/components/logos/LogoDivTrak.vue';
-
-const authStore = useAuthStore();
-
-const loginData = reactive({ phone: '', pin: '' });
-
-function onSubmit () {
-  authStore.login(loginData);
-}
+import FormLogin from '@/components/forms/FormLogin.vue';
 </script>
 
 <template>
-  <main class="login">
-    <h1>Please log in</h1>
-
-    <div class="grid">
-      <LogoDivTrak :width="250" />
-
-      <form @submit.prevent="onSubmit">
-        <label for="phone">
-          <div class="label">Phone</div>
-          <input v-model="loginData.phone" id="phone" type="text" placeholder="+49123456789">
-        </label>
-        <label for="pin">
-          <div class="label">PIN</div>
-          <input v-model="loginData.pin" id="pin" type="text" placeholder="8888">
-        </label>
-        <button>Einloggen</button>
-    </form>
-  </div>
+  <main class="view-login">
+    <FormLogin />
   </main>
 </template>
 
 <style scoped>
-.login {
-  max-inline-size: 50%;
-  margin-inline: auto;
-}
-
-.grid {
-  display: grid;
-  grid-template-columns: 50% 50%;
-  gap: 1rem;
-  place-items: center;
-  place-content: center;
-}
 </style>
