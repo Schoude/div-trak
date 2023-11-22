f6bf47410a420c54513d719ba1f8d2e6b681f9a9
Marc <marcbaque1311@gmail.com>
2023-11-22 20:14:23 +0100

    [ ADD ] app layout for content- view

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,11 @@
   <RouterView />
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+#app {
+  &:has(.content-view) {
+    display: grid;
+    grid-template-rows: 4.5rem 1fr 4.5rem;
+  }
+}
 </style>
