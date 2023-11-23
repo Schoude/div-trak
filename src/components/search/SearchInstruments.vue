81a92155a9b006c601f710ac1d5aaaeeba01dabc
Marc <marcbaque1311@gmail.com>
2023-11-23 22:46:28 +0100

    [ ADD ] clicked outside handler for instrument search

--- src/components/search/SearchInstruments.vue
+++ src/components/search/SearchInstruments.vue
@@ -1,9 +1,13 @@
 <script setup lang='ts'>
 import InputSearch from '@/components/search/InputSearch.vue';
 import SearchResults from '@/components/search/SearchResults.vue';
+import { onClickOutside } from '@vueuse/core';
 import { ref } from 'vue';
 
+const searchInstruments = ref<HTMLElement | null>(null);
 const resultsVisible = ref(false);
+
+onClickOutside(searchInstruments, () => resultsVisible.value = false);
 
 function onFocusInput () {
   if (resultsVisible.value) {
@@ -15,7 +19,7 @@ function onFocusInput () {
 </script>
 
 <template>
-<div class="search-instruments">
+<div ref="searchInstruments" class="search-instruments">
   <InputSearch @focus:input="onFocusInput" />
   <SearchResults v-if="resultsVisible" />
 </div>
