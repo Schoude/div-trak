c32da8508e557f18e02f9db73e8fb8e1de2f5dde
Marc <marcbaque1311@gmail.com>
2023-11-24 23:33:49 +0100

    [ ADD ] types for instruments

---
+++ src/types/tr/events/instruments.ts
@@ -0,0 +1,42 @@
+export interface InstrumentEvent {
+  exchangeIds: string[];
+  dividends: any[];
+  splits: Split[];
+  name: string;
+  typeId: string;
+  isin: string;
+  shortName: string;
+  intlSymbol: string;
+  tags: Tag[];
+  company: Company;
+  marketCap: MarketCap;
+  lastDividend: null;
+  fundInfo: null;
+  imageId: string;
+  description: string | null;
+}
+
+interface Company {
+  name: string;
+  description: null;
+  ipoDate: number;
+  countryOfOrigin: string;
+}
+
+interface MarketCap {
+  value: string;
+}
+
+interface Split {
+  id: string;
+  date: number;
+  initial: number;
+  final: number;
+}
+
+interface Tag {
+  type: string;
+  id: string;
+  name: string;
+  icon: string;
+}
---
+++ src/types/tr/events/stock-details.ts
@@ -0,0 +1,109 @@
+export interface StockDetailsEvent {
+  isin: string;
+  company: Company;
+  similarStocks: SimilarStock[];
+  dividends: Dividend[];
+  totalDivendendCount: number;
+  events: Event[];
+  pastEvents: Event[];
+  analystRating: AnalystRating;
+  aggregatedDividends: AggregatedDividend[];
+  dividendFrequency: string;
+}
+
+interface AggregatedDividend {
+  periodStartDate: string;
+  projected: boolean;
+  yieldValue: number;
+  amount: number;
+  count: number;
+  price: number;
+}
+
+interface AnalystRating {
+  targetPrice: TargetPrice;
+  recommendations: Recommendations;
+}
+
+interface Recommendations {
+  buy: number;
+  outperform: number;
+  hold: number;
+  underperform: number;
+  sell: number;
+}
+
+interface TargetPrice {
+  average: number;
+  high: number;
+  low: number;
+}
+
+interface Company {
+  name: string;
+  description: string;
+  yearFounded: number;
+  tickerSymbol: string;
+  peRatioSnapshot: number;
+  pbRatioSnapshot: number;
+  dividendYieldSnapshot: number;
+  earningsCall: null;
+  marketCapSnapshot: number;
+  dailyCloseYearSD: null;
+  beta: number;
+  countryCode: string;
+  ceoName: string;
+  cfoName: string;
+  cooName: null;
+  employeeCount: number;
+  eps: number;
+}
+
+interface Dividend {
+  id: string;
+  paymentDate: string;
+  recordDate: string;
+  exDate: string;
+  amount: number;
+  yield: null;
+  type: DividendType;
+}
+
+enum DividendType {
+  Cash = 'cash',
+  Unknown = 'unknown',
+}
+
+interface Event {
+  id: string;
+  title: Title;
+  timestamp: number;
+  description: string;
+  dividend: Dividend | null;
+}
+
+enum Title {
+  AnalystPresentation = 'Analyst  Presentation',
+  Dividende = 'Dividende',
+  EarningsCall = 'Earnings Call',
+  FinancialsRelease = 'Financials Release',
+  Hauptversammlung = 'Hauptversammlung',
+}
+
+interface SimilarStock {
+  isin: string;
+  name: string;
+  tags: Tag[];
+}
+
+interface Tag {
+  type: TagType;
+  id: string;
+  name: string;
+  icon: string;
+}
+
+enum TagType {
+  Country = 'country',
+  Sector = 'sector',
+}
