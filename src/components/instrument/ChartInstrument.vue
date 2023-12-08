2356692e76a14c05a21f026627cd4f5c38ad19f2
Marc Baque <m.baque@inreal-tech.com>
2023-12-08 16:55:38 +0100

    [ ADD ] initial candlestick chart

--- src/components/instrument/ChartInstrument.vue
+++ src/components/instrument/ChartInstrument.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { defaultFormat, timeformat } from '@/utils/visus';
-import { axisBottom, axisLeft, max, min, scaleLinear, scaleLog, scaleUtc, select } from 'd3';
+import { axisBottom, axisLeft, format, max, min, scaleBand, scaleLinear, scaleLog, schemeSet1, select, utcFormat, utcMinutes } from 'd3';
 import { onMounted, ref } from 'vue';
 import type { AggregateHistory } from './aggregateHistory';
 
@@ -12,12 +12,12 @@ const chart = ref<HTMLElement | null>(null);
 
 const margin = {
   top: 16,
-  right: 30,
+  right: 0,
   bottom: 24,
   left: 50,
 };
 const width = 1200 - margin.left - margin.right;
-const height = 280 - margin.top - margin.bottom;
+const height = 360 - margin.top - margin.bottom;
 
 function drawChart () {
   const svg = select(chart.value)
@@ -29,7 +29,7 @@ function drawChart () {
 
   // Y-Axis
   const y = scaleLog()
-    // @ts-expect-error bad libtypes
+    // @ts-expect-error bad lib types
     .domain([
       min(props.ticker.aggregates, d => +d.low),
       max(props.ticker.aggregates, d => +d.high),
@@ -45,19 +45,62 @@ function drawChart () {
       .attr('x2', width))
     .call(g => g.select('.domain').remove());
 
+
   // X-Axis
-  const x = scaleUtc()
+  // Minutes in 10 minute interval
+  const xDomain = utcMinutes(
+    new Date(props.ticker.aggregates.at(0)!.time),
+    new Date(props.ticker.expectedClosingTime),
+    10,
+  );
+  const x = scaleBand()
+    // @ts-expect-error bad lib types
+    .domain(xDomain)
     .range([0, width])
-    .domain([
-      new Date(props.ticker.aggregates.at(0)!.time),
-      new Date(props.ticker.expectedClosingTime),
-    ]);
+    .padding(0.45);
 
   svg.append('g')
     .attr('transform', `translate(0,${height})`)
     // @ts-expect-error bad lib types
     .call(axisBottom(x).tickFormat(timeformat.format('%H:%M')))
     .call(g => g.select('.domain').remove());
+
+  // Candles
+  const g = svg.append('g')
+    .attr('stroke-linecap', 'round')
+    .attr('stroke', 'black')
+    .selectAll('g')
+    .enter()
+    .data(props.ticker.aggregates)
+    .join('g')
+    // @ts-expect-error bad lib types
+    .attr('transform', d => `translate(${x(d.time)},0)`);
+
+  // Whiskers
+  g.append('line')
+    .attr('y1', d => y(+d.low))
+    .attr('y2', d => y(+d.high))
+    .attr('stroke', 'white');
+
+  // Candle
+  g.append('line')
+    .attr('y1', d => y(+d.open))
+    .attr('y2', d => y(+d.close))
+    .attr('stroke-width', x.bandwidth())
+    .attr('stroke', d => d.open > d.close ? schemeSet1[0]
+      : d.close > d.open ? schemeSet1[2]
+        : schemeSet1[8]);
+
+  // Information
+  const formatDate = utcFormat('%B %-d, %Y');
+  const formatValue = defaultFormat.format('$.2f');
+
+  g.append('title')
+    .text(d => `${formatDate(new Date(d.time))}
+Open: ${formatValue(+d.open)}
+Close: ${formatValue(+d.close)} (${format('+.2%')(1 - (+d.open / +d.close))})
+Low: ${formatValue(+d.low)}
+High: ${formatValue(+d.high)}`);
 }
 
 onMounted(() => {
@@ -70,3 +113,17 @@ onMounted(() => {
     <div ref="chart" class="chart"></div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.chart {
+  &:deep(svg) {
+    .tick {
+      text {
+        font-size: 13px;
+        font-family: var(--font-family);
+      }
+    }
+
+  }
+}
+</style>
