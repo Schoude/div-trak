95bd39c6eb4e416a1b65a0c7e9956468b6e21e05
Marc Baque <m.baque@inreal-tech.com>
2023-12-15 09:24:56 +0100

    [ ADD ] DividendScraper component

---
+++ src/components/dividends/DividendScraper.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { supabase } from '@/supabase/client';
+import type { DividendsCrawlReturnType } from '@/supabase/types/functions';
+import type { Dividend } from '@/types/tr/events/stock-details';
+import { computed, nextTick, onUnmounted, ref, watch } from 'vue';
+import ButtonAction from '../buttons/ButtonAction.vue';
+import InputText from '../inputs/InputText.vue';
+
+const props = defineProps<{
+  isLoading: boolean;
+  stockName: string;
+}>();
+
+const emit = defineEmits<{
+  loading: [isLoading: boolean];
+  'update:scraped-dividends': [dividends: Dividend[]];
+  scrapeCompleted: [boolean];
+}>();
+
+const baseUrl = 'https://dividendhistory.org/payout';
+const symbolToScrape = ref('');
+
+const canScrapeDividends = computed(() => !props.isLoading && symbolToScrape.value !== '');
+const urlToScrape = computed(() => `${baseUrl}/${symbolToScrape.value}`);
+
+watch(symbolToScrape, () => {
+  symbolToScrape.value = symbolToScrape.value.toUpperCase();
+});
+
+onUnmounted(() => {
+  symbolToScrape.value = '';
+});
+
+async function onScrapeSubmit () {
+  emit('loading', true);
+
+  try {
+    const crawlDividendsResponse = await supabase
+      .functions
+      .invoke<DividendsCrawlReturnType>('dividends-crawl', {
+        body: {
+          url: urlToScrape.value,
+        },
+      });
+
+    if (crawlDividendsResponse.error) throw crawlDividendsResponse.error;
+
+    if (crawlDividendsResponse.data) {
+      // @ts-expect-error bad DOM types
+      document.startViewTransition(async () => {
+        // Emit values for successful crawl
+        emit('update:scraped-dividends', crawlDividendsResponse.data.estimatedDividends);
+        emit('scrapeCompleted', true);
+
+        await nextTick();
+      });
+    } else throw new Error('Response has no data: Function: order-delete');
+
+    if (!crawlDividendsResponse.data?.estimatedDividends) {
+      console.error(`Error crawling the estimated dividends for ${props.stockName} (${symbolToScrape.value})`);
+
+      return;
+    }
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    emit('loading', false);
+  }
+}
+</script>
+
+<template>
+  <div class="dividend-scraper">
+    <iframe :src="baseUrl" frameborder="0"></iframe>
+
+    <form class="form-crawl-dividends" @submit.prevent="onScrapeSubmit">
+      <InputText id="symbol-name" v-model.trim="symbolToScrape" placeholder="Symbol to crawl i.e. MSFT"
+        autocomplete="off" />
+      <ButtonAction variant="dawn" :disabled="!canScrapeDividends">
+        Crawl Dividends
+      </ButtonAction>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.dividend-scraper {
+  display: flex;
+  flex-direction: column;
+
+  iframe {
+    flex: 1;
+  }
+}
+
+.form-crawl-dividends {
+  padding: 1.5rem;
+  display: grid;
+  gap: 1rem;
+}
+
+@container modal-dividend-history (width > 768px) {
+  .form-crawl-dividends {
+    padding: 2rem;
+    grid-template-rows: 1fr;
+    grid-template-columns: 15% 1fr;
+    gap: 2rem;
+  }
+}
+</style>
--- src/components/modals/ModalDividendHistory.vue
+++ src/components/modals/ModalDividendHistory.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
 import ButtonAction from '@/components/buttons/ButtonAction.vue';
-import InputText from '@/components/inputs/InputText.vue';
 import DividendListItem from '@/components/lists/DividendListItem.vue';
 import ModalBase from '@/components/modals/ModalBase.vue';
-import { supabase } from '@/supabase/client';
-import type { DividendsCrawlReturnType } from '@/supabase/types/functions';
 import type { Dividend } from '@/types/tr/events/stock-details';
-import { computed, nextTick, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
+import DividendScraper from '../dividends/DividendScraper.vue';
 
 const root = ref<typeof ModalBase | null>(null);
 const modalIsOpen = ref(false);
 
-const baseUrl = 'https://dividendhistory.org/payout';
-const symbolToCrawl = ref('');
 const isLoading = ref(false);
-const crawlCompleted = ref(false);
-const crawledDividends = ref<Dividend[]>([]);
+const scrapeCompleted = ref(false);
+const scrapedDividends = ref<Dividend[]>([]);
 
-watch(symbolToCrawl, () => {
-  symbolToCrawl.value = symbolToCrawl.value.toUpperCase();
-});
-
-const props = defineProps<{
+defineProps<{
   stockName: string;
 }>();
 
@@ -29,9 +21,7 @@ defineExpose({
   onOpenIframeModalOpen,
 });
 
-const canCrawlDividends = computed(() => !isLoading.value && symbolToCrawl.value !== '');
-const canConfirmDividends = computed(() => !isLoading.value && crawlCompleted.value && crawledDividends.value.length > 0);
-const urlToCrawl = computed(() => `${baseUrl}/${symbolToCrawl.value}`);
+const canConfirmDividends = computed(() => !isLoading.value && scrapeCompleted.value && scrapedDividends.value.length > 0);
 
 function onOpenIframeModalOpen () {
   root.value?.$el.showModal();
@@ -48,46 +38,10 @@ function onOpenIframeModalClose () {
   resetValues();
 }
 
-async function onCrawlSubmit () {
-  isLoading.value = true;
-  try {
-    const crawlDividendsResponse = await supabase.functions.invoke<DividendsCrawlReturnType>('dividends-crawl', {
-      body: {
-        url: urlToCrawl.value,
-      },
-    });
-
-    if (crawlDividendsResponse.error) throw crawlDividendsResponse.error;
-
-    if (crawlDividendsResponse.data) {
-      // @ts-expect-error bad DOM types
-      document.startViewTransition(async () => {
-        // Set values for successful crawl
-        crawlCompleted.value = true;
-        crawledDividends.value = crawlDividendsResponse.data.estimatedDividends;
-
-        await nextTick();
-      });
-    } else throw new Error('Response has no data: Function: order-delete');
-
-    if (!crawlDividendsResponse.data?.estimatedDividends) {
-      console.error(`Error crawling the estimated dividends for ${props.stockName} (${symbolToCrawl.value})`);
-
-      return;
-    }
-
-  } catch (error) {
-    console.error(error);
-  } finally {
-    isLoading.value = false;
-  }
-}
-
 function resetValues () {
-  symbolToCrawl.value = '';
   isLoading.value = false;
-  crawlCompleted.value = false;
-  crawledDividends.value = [];
+  scrapeCompleted.value = false;
+  scrapedDividends.value = [];
 }
 </script>
 
@@ -96,24 +50,15 @@ function resetValues () {
     <template #title>dividendhistory.org | {{ stockName }}</template>
     <template #content>
       <div v-if="modalIsOpen" class="inner">
-        <!-- TODO: move into component -->
-        <div v-if="!crawlCompleted" class="dividend-finder">
-          <iframe :src="baseUrl" frameborder="0"></iframe>
-
-          <form class="form-crawl-dividends" @submit.prevent="onCrawlSubmit">
-            <InputText id="symbol-name" v-model.trim="symbolToCrawl" placeholder="Symbol to crawl i.e. MSFT"
-              autocomplete="off" />
-            <ButtonAction variant="dawn" :disabled="!canCrawlDividends">
-              Crawl Dividends
-            </ButtonAction>
-          </form>
-        </div>
+        <DividendScraper v-if="!scrapeCompleted" :is-loading="isLoading" :stock-name="stockName"
+          @loading="e => isLoading = e" @scrape-completed="e => scrapeCompleted = e"
+          @update:scraped-dividends="e => scrapedDividends = e" />
 
         <!-- TODO: move into component -->
         <div v-else class="dividend-confirmation">
           <div class="crawled-dividends">
             <ul>
-              <DividendListItem v-for="dividend of crawledDividends" :dividend="dividend" :key="dividend.id">
+              <DividendListItem v-for="dividend of scrapedDividends" :dividend="dividend" :key="dividend.id">
                 <template #action>
                   <div class="confirmation">
                     <ButtonAction variant="dawn" :disabled="!canConfirmDividends">
@@ -146,25 +91,8 @@ iframe {
   display: flex;
 }
 
-.dividend-finder {
+.dividend-scraper {
   flex: 1;
-  display: flex;
-  flex-direction: column;
-}
-
-.form-crawl-dividends {
-  padding: 1.5rem;
-  display: grid;
-  gap: 1rem;
-}
-
-@container modal-dividend-history (width > 768px) {
-  .form-crawl-dividends {
-    padding: 2rem;
-    grid-template-rows: 1fr;
-    grid-template-columns: 15% 1fr;
-    gap: 2rem;
-  }
 }
 
 .dividend-confirmation {
