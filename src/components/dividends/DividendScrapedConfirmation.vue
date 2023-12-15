c0564cd0bef1a7f1b8059d74fd2f60e10381fadf
Marc Baque <m.baque@inreal-tech.com>
2023-12-15 11:35:08 +0100

    [ ADD ] upsert scraped dividends

--- src/components/dividends/DividendScrapedConfirmation.vue
+++ src/components/dividends/DividendScrapedConfirmation.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import ButtonAction from '@/components/buttons/ButtonAction.vue';
 import DividendListItem from '@/components/lists/DividendListItem.vue';
-import type { Dividend } from '@/types/tr/events/stock-details';
+import { supabase } from '@/supabase/client';
+import type { DbResult } from '@/supabase/types/helpers';
+import { DividendType, type Dividend } from '@/types/tr/events/stock-details';
 import { computed } from 'vue';
 
 const props = defineProps<{
@@ -11,6 +13,38 @@ const props = defineProps<{
 }>();
 
 const canConfirmDividends = computed(() => !props.isLoading && props.scrapedDividends.length > 0);
+
+async function onConfirmDividendClick (dividend: Dividend) {
+  const upsertScrapedDividendQuery = supabase
+    .from('dividends_scraped')
+    .upsert({
+      isin: props.isin,
+      isin_ex_date: `${props.isin}-${dividend.exDate}`,
+      ex_date: dividend.exDate,
+      record_date: dividend.recordDate,
+      payment_date: dividend.paymentDate,
+      amount: dividend.amount,
+      information: dividend.information,
+      type: DividendType.Cash,
+    }, {
+      onConflict: 'isin_ex_date',
+    })
+    .select()
+    .single();
+
+  const uptedPortfolioResult: DbResult<typeof upsertScrapedDividendQuery> =
+    await upsertScrapedDividendQuery;
+
+  if (uptedPortfolioResult.error) {
+    console.error(uptedPortfolioResult.error);
+
+    return;
+  }
+
+  // TODO: add to to seperate pinia store dividends-scraped. they should be merged into the other dividend arrays
+  // and also loaded after use login
+  console.log(uptedPortfolioResult.data);
+}
 </script>
 
 <template>
@@ -23,7 +57,8 @@ const canConfirmDividends = computed(() => !props.isLoading && props.scrapedDivi
         <DividendListItem v-for="dividend of scrapedDividends" :dividend="dividend" :key="dividend.id">
           <template #action>
             <div class="confirmation">
-              <ButtonAction class="button-dividend-confirm" variant="dawn" :disabled="!canConfirmDividends">
+              <ButtonAction class="button-dividend-confirm" variant="dawn" :disabled="!canConfirmDividends"
+                @click="onConfirmDividendClick(dividend)">
                 Confirm dividend
               </ButtonAction>
             </div>
