0a7401ef34291aa97d92681c97fdea26a665ed08
Marc <marcbaque1311@gmail.com>
2024-01-08 21:29:07 +0100

    [ ADD ] google deps

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,8 @@ interface ImportMetaEnv {
   readonly VITE_TR_CLIENT_ID: string
   readonly VITE_TR_SOCKET_URL: string
   readonly VITE_TR_IMAGE_ASSETS_BASE: string
+  readonly VITE_OAUTH_CLIENT_ID: string
+  readonly VITE_GOOGLE_API_KEY: string
 }
 
 interface ImportMeta {
